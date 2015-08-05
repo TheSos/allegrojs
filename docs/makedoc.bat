43a6c82c669c06994be9c3debbdc8eaef81bac6b
TheSos <just.sos.it@gmail.com>
2015-08-05 23:09:22 +0200

    Doc path fix

--- docs/makedoc.bat
+++ docs/makedoc.bat
@@ -2,7 +2,7 @@
 echo Generating docs for jallegro.js...
 doxygen Doxyfile
 echo Cleaning up....
-copy doc\html\jallegro_8js.html docs\index.html
+copy doc\html\jallegro_8js.html index.html
 del /s /f /q doc\*.*
 rd doc\html\search
 rd doc\html
