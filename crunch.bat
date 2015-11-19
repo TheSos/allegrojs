ca5cea86e180f2fe8eedbdec831605be654e3a3b
TheSos <just.sos.it@gmail.com>
2015-11-19 22:13:21 +0100

    Delete crunch.bat

--- crunch.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Crunching allegro.js
-java -jar compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --generate_exports --js allegro.js --js_output_file alleg.js
-echo Written crunched allegro to alleg.js
